f48a0c4fd5f67d8782fa167eb7441636d99e4eb1
x1mukesh <of.mukesh@gmail.com>
2023-02-02 00:13:28 +0530

    applications

--- applications/urls.py
+++ applications/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.applications, name='applications'),
+    path('<str:pk>/', views.view_application, name='view_application'),
+    path('<str:pk>/<slug:status>/',
+         views.update_application, name='update_application'),
 ]
--- applications/views.py
+++ applications/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Application
 from django.http import HttpResponseNotFound
+from django.contrib import messages
 
 
 @login_required(login_url='/auth/login/')
@@ -10,4 +11,22 @@ def applications(request):
         context = {'applications': Application.objects.all().filter(
             status="Pending")}
         return render(request, 'pages/applications.html', context)
+    return HttpResponseNotFound()
+
+
+@login_required(login_url='/auth/login/')
+def view_application(request, pk):
+    if request.user.is_staff:
+        context = {'application': Application.objects.get(trace_id=pk)}
+        return render(request, 'pages/view_application.html', context)
+    return HttpResponseNotFound()
+
+
+@login_required(login_url='/auth/login/')
+def update_application(request, pk, status):
+    if request.user.is_staff:
+        print(status)
+        context = {'application': Application.objects.get(trace_id=pk)}
+        messages.info(request, 'done')
+        return HttpResponse("done", status=200)
     return HttpResponseNotFound()
