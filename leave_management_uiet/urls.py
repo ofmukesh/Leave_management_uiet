9f4874dc22ea3e025ad59cee36b456d4c28128d7
x1mukesh <of.mukesh@gmail.com>
2023-02-05 01:15:23 +0530

    reset pass andprofile done

--- leave_management_uiet/urls.py
+++ leave_management_uiet/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
 from home.views import home
+from django.contrib.auth import views as auth_views
 
 admin.site.site_header = "University Institute of Engg. & Technology"
 admin.site.site_title = "Uiet"
@@ -14,4 +15,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
     path('account/', include('accounts.urls')),
+    path('accounts/reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='forms/reset_pass_form.html'), name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='pages/reset_pass_done.html'),
+         name='password_reset_complete'),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
